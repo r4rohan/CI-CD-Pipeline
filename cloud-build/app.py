8b119481929ae219efa55e60164bc2404bec3d44
Rohan Singh <30438229+r4rohan@users.noreply.github.com>
2020-08-17 22:11:11 +0530

    Add files via upload

--- cloud-build/app.py
+++ cloud-build/app.py
@@ -7,5 +7,5 @@ def hello():
     return "Stay inside, stay safe and keep social distancing. \nThis message is coming from CloudBuild"
 
 if __name__ == '__main__':
- app.run(debug=True,host='0.0.0.0',port=5000)
+ app.run(debug=True,host='0.0.0.0',port=5001)
 app.run(host='0.0.0.0') 
